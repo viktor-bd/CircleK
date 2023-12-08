c2bd8fb15cd791aa6e01b61157dda962951bdb15
rasmuslarsendma <115786715+rasmuslarsendma@users.noreply.github.com>
2023-12-08 13:18:48 +0100

    Empty PersonCTR

---
+++ src/control/PersonController.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package control;
+
+/**
+ * @author 
+ *
+ */
+public class PersonController {
+
+	/**
+	 * 
+	 */
+	public PersonController() {
+		// TODO Auto-generated constructor stub
+	}
+
+}

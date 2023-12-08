57f645c943996d7580136dd1cf1ebeb1b8b0ebbb
rasmuslarsendma <115786715+rasmuslarsendma@users.noreply.github.com>
2023-12-08 13:20:19 +0100

    Create Supplier.java
    List of ingredients?
    1:1

---
+++ src/model/Supplier.java
@@ -0,0 +1,132 @@
+/**
+ * 
+ */
+package model;
+
+import java.util.ArrayList;
+
+/**
+ * @author Rasmus Larsen, Viktor Dorph, Johannes Jensen, Malik Agerbæk, Shemon
+ *         Chowdhury
+ *
+ */
+public class Supplier {
+	private int supplierId;
+	private String address;
+	private String country;
+	private String email;
+	private int phoneNumber;
+	private String name;
+	private ArrayList<Ingredient> ingredients; 
+
+	// Constructor
+	public Supplier(int supplierId, String address, String country, String email, int phoneNumber, String name) {
+		this.supplierId = supplierId;
+		this.address = address;
+		this.country = country;
+		this.email = email;
+		this.phoneNumber = phoneNumber;
+		this.name = name;
+		this.ingredients = new ArrayList<>();
+	}
+
+	/**
+	 * @return the supplierId
+	 */
+	public int getSupplierId() {
+		return supplierId;
+	}
+
+	/**
+	 * @param supplierId the supplierId to set
+	 */
+	public void setSupplierId(int supplierId) {
+		this.supplierId = supplierId;
+	}
+
+	/**
+	 * @return the address
+	 */
+	public String getAddress() {
+		return address;
+	}
+
+	/**
+	 * @param address the address to set
+	 */
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	/**
+	 * @return the country
+	 */
+	public String getCountry() {
+		return country;
+	}
+
+	/**
+	 * @param country the country to set
+	 */
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	/**
+	 * @return the email
+	 */
+	public String getEmail() {
+		return email;
+	}
+
+	/**
+	 * @param email the email to set
+	 */
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	/**
+	 * @return the phoneNumber
+	 */
+	public int getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	/**
+	 * @param phoneNumber the phoneNumber to set
+	 */
+	public void setPhoneNumber(int phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the ingredients
+	 */
+	public ArrayList<Ingredient> getIngredients() {
+		return ingredients;
+	}
+
+	/**
+	 * @param ingredients the ingredients to set
+	 */
+	public void setIngredients(ArrayList<Ingredient> ingredients) {
+		this.ingredients = ingredients;
+	}
+}
+
+

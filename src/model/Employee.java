5ef592cc68d0850ad32bc8d6a8a4956b476b9c6e
rasmuslarsendma <115786715+rasmuslarsendma@users.noreply.github.com>
2023-12-08 13:19:34 +0100

    Create Employee.java

---
+++ src/model/Employee.java
@@ -0,0 +1,44 @@
+/**
+ * 
+ */
+package model;
+
+/**
+ * @author Rasmus Larsen, Viktor Dorph, Johannes Jensen, Malik Agerbæk, Shemon Chowdhury 
+ *
+ */
+public class Employee extends Person {
+    private int employeeId; //TODO CHECK IF WE NEED TO DUPLICATE PERSONID TO EMPLOYEE ID
+    private boolean isManager; // true for manager, false for salesassistent
+
+    // Constructor
+    public Employee(int personId, String firstName, String lastName, String phoneNumber,
+                    String zipcode, String email, String address, boolean isManager) {
+        super(personId, firstName, lastName, phoneNumber, zipcode, email, address);
+        this.isManager = isManager;
+    }
+
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+    
+    public void setEmployeeId(int employeeId) {
+        this.employeeId = employeeId;
+    }
+
+   /**
+    * 
+    * @return true if employee is a manager, false if not.
+    */
+    public boolean isManager() {
+        return isManager;
+    }
+    // Changes the boolean state of isManager 
+    public void setManager(boolean manager) {
+        isManager = manager;
+    }
+
+  
+}
+

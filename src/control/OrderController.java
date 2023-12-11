5fcae9fff31df339ea0c43be141f7f72874e3c27
viktor-bd <viktordorph@hotmail.com>
2023-12-11 12:44:53 +0100

    OrderController linebreaks

--- src/control/OrderController.java
+++ src/control/OrderController.java
@@ -11,19 +11,19 @@ public class OrderController {
 
 
     public OrderController(OrderDB orderDB) {
-        this.orderDB =  new OrderDB();
+        this.orderDB = new OrderDB();
     }
 
     public Order createOrder(int orderID, boolean pickUpStatus, LocalDateTime pickupDate, boolean isPaid, Customer customer, Employee employee) {
         Order newOrder = new Order(orderID, pickUpStatus, pickupDate, isPaid, customer, employee);
         return newOrder;
     }
+
     public Order saveOrder(Order order) {
         orderDB.saveOrder(order); // Call orderDAL to save the order
         return order;
     }
-    public void addOrderLineToOrder(OrderLine orderLine, Order order) {
-    order.addOrderLine(orderLine);
+
     }
     public OrderLine createOrderLine (Product product, int quantity){
         OrderLine orderLine = new OrderLine(quantity, product);
@@ -31,7 +31,7 @@ public class OrderController {
     }
 
     //TODO orderID skal hentes fra DB via GUI kald til CTRL som går til DAL
-    public void confirmOrder(Order order){
+    public void confirmOrder(Order order) {
         // Logic for confirming an order
         order.setPickUpStatus(true);
         // Add logic to update the order directly in the database or perform necessary operations

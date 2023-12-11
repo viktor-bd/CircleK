324cb64cf3ec1dc01b16fac5cfb23f7ebbe1fad9
rasmuslarsendma <115786715+rasmuslarsendma@users.noreply.github.com>
2023-12-11 11:07:29 +0100

    Product View Start

---
+++ src/gui/ProductView.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package gui;
+
+/**
+ * @author Rasmus Larsen, Viktor Dorph, Johannes Jensen, Malik Agerbæk, Shemon Chowdhury 
+ *
+ */
+public class ProductView {
+
+	/**
+	 * 
+	 */
+	public ProductView() {
+		// TODO Auto-generated constructor stub
+	}
+
+}

93f181cd2e9ce6b54132ccd09b70b7f286d6eff0
rasmuslarsendma <115786715+rasmuslarsendma@users.noreply.github.com>
2023-12-12 19:41:33 +0100

    GUI Design ProductView

--- src/gui/ProductView.java
+++ src/gui/ProductView.java
@@ -7,6 +7,9 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import java.awt.GridLayout;
+import javax.swing.JScrollPane;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
 
 /**
  * @author Rasmus Larsen, Viktor Dorph, Johannes Jensen, Malik Agerbæk, Shemon
@@ -14,16 +17,63 @@ import java.awt.GridLayout;
  *
  */
 public class ProductView extends JFrame {
+	private JTextField textField;
 
 	
 	/**
 	 * 
 	 */
+	//TODO Indsæt date med i constructor
 	public ProductView() {
+		setTitle("Opret ordre forespørgsel");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 500, 300);
 		getContentPane().setLayout(new GridLayout(1, 0, 0, 0));
 		
 		JPanel panel = new JPanel();
 		getContentPane().add(panel);
+		panel.setLayout(null);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(10, 28, 108, 207);
+		panel.add(scrollPane);
+		
+		JLabel lblNewLabel = new JLabel("Produkter");
+		lblNewLabel.setBounds(10, 11, 108, 14);
+		panel.add(lblNewLabel);
+		
+		JButton btnNewButton = new JButton("Tilføj linjer");
+		btnNewButton.setBounds(117, 123, 96, 23);
+		panel.add(btnNewButton);
+		
+		JScrollPane scrollPane_1 = new JScrollPane();
+		scrollPane_1.setBounds(213, 28, 118, 207);
+		panel.add(scrollPane_1);
+		
+		textField = new JTextField();
+		textField.setBounds(242, 236, 89, 14);
+		panel.add(textField);
+		textField.setColumns(10);
+		
+		JButton btnNewButton_1 = new JButton("Tilføj kunde");
+		btnNewButton_1.setBounds(335, 28, 89, 23);
+		panel.add(btnNewButton_1);
+		
+		JButton btnNewButton_2 = new JButton("Godkend ");
+		btnNewButton_2.setBounds(335, 220, 89, 30);
+		panel.add(btnNewButton_2);
+		
+		JButton btnNewButton_3 = new JButton("Tilbage");
+		btnNewButton_3.setBounds(335, 51, 89, 23);
+		panel.add(btnNewButton_3);
+		
+		JLabel lblNewLabel_1 = new JLabel("Ordrelinjer");
+		lblNewLabel_1.setBounds(213, 11, 118, 14);
+		panel.add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_2 = new JLabel("Pris:");
+		lblNewLabel_2.setBounds(213, 236, 46, 14);
+		panel.add(lblNewLabel_2);
 
 	}
 }

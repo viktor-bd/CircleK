b657754945bf218a0997ece444f92705b93d192d
rasmuslarsendma <115786715+rasmuslarsendma@users.noreply.github.com>
2023-12-12 22:36:59 +0100

    Create UnconfirmedOrderView.java

---
+++ src/gui/UnconfirmedOrderView.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package gui;
+
+import javax.swing.JFrame;
+
+/**
+ * @author Rasmus Larsen, Viktor Dorph, Johannes Jensen, Malik Agerbæk, Shemon Chowdhury 
+ *
+ */
+public class UnconfirmedOrderView extends JFrame {
+
+	/**
+	 * 
+	 */
+	public UnconfirmedOrderView() {
+		// TODO Auto-generated constructor stub
+	}
+
+}

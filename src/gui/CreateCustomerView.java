09e7797ecce69892e2c0c15d35c6d6b5090f1c97
rasmuslarsendma <115786715+rasmuslarsendma@users.noreply.github.com>
2023-12-12 22:34:51 +0100

    Create CreateCustomerView.java

---
+++ src/gui/CreateCustomerView.java
@@ -0,0 +1,132 @@
+/**
+ * 
+ */
+package gui;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import javax.swing.JTextField;
+
+/**
+ * @author Rasmus Larsen, Viktor Dorph, Johannes Jensen, Malik Agerbæk, Shemon Chowdhury 
+ *
+ */
+public class CreateCustomerView extends JFrame {
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
+	private JTextField textField_3;
+	private JTextField textField_4;
+	private JTextField textField_5;
+	private JTextField textField_6;
+	private JTextField textField_7;
+	private JTextField textField_8;
+
+	/**
+	 * 
+	 */
+	public CreateCustomerView() {
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		JPanel panel = new JPanel();
+		getContentPane().add(panel, BorderLayout.CENTER);
+		panel.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Kundeinformationer");
+		lblNewLabel.setBounds(10, 11, 103, 21);
+		panel.add(lblNewLabel);
+		
+		JButton btnNewButton = new JButton("Opret");
+		btnNewButton.setBounds(335, 227, 89, 23);
+		panel.add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("Afbryd");
+		btnNewButton_1.setBounds(335, 205, 89, 23);
+		panel.add(btnNewButton_1);
+		
+		JLabel lblNewLabel_1 = new JLabel("Fornavn");
+		lblNewLabel_1.setBounds(10, 43, 64, 14);
+		panel.add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_2 = new JLabel("Efternavn");
+		lblNewLabel_2.setBounds(10, 76, 64, 14);
+		panel.add(lblNewLabel_2);
+		
+		JLabel lblNewLabel_3 = new JLabel("Telefon");
+		lblNewLabel_3.setBounds(10, 113, 64, 14);
+		panel.add(lblNewLabel_3);
+		
+		JLabel lblNewLabel_4 = new JLabel("E-mail");
+		lblNewLabel_4.setBounds(10, 152, 64, 14);
+		panel.add(lblNewLabel_4);
+		
+		JLabel lblNewLabel_5 = new JLabel("CVR");
+		lblNewLabel_5.setBounds(10, 199, 64, 14);
+		panel.add(lblNewLabel_5);
+		
+		JLabel lblNewLabel_6 = new JLabel("Vejnavn");
+		lblNewLabel_6.setBounds(173, 43, 46, 14);
+		panel.add(lblNewLabel_6);
+		
+		JLabel lblNewLabel_7 = new JLabel("Husnummer");
+		lblNewLabel_7.setBounds(173, 76, 64, 14);
+		panel.add(lblNewLabel_7);
+		
+		JLabel lblNewLabel_8 = new JLabel("Postnr.");
+		lblNewLabel_8.setBounds(173, 113, 46, 14);
+		panel.add(lblNewLabel_8);
+		
+		JLabel lblNewLabel_9 = new JLabel("By");
+		lblNewLabel_9.setBounds(173, 152, 46, 14);
+		panel.add(lblNewLabel_9);
+		
+		textField = new JTextField();
+		textField.setBounds(77, 40, 86, 20);
+		panel.add(textField);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(77, 73, 86, 20);
+		panel.add(textField_1);
+		textField_1.setColumns(10);
+		
+		textField_2 = new JTextField();
+		textField_2.setBounds(77, 110, 86, 20);
+		panel.add(textField_2);
+		textField_2.setColumns(10);
+		
+		textField_3 = new JTextField();
+		textField_3.setBounds(77, 152, 86, 20);
+		panel.add(textField_3);
+		textField_3.setColumns(10);
+		
+		textField_4 = new JTextField();
+		textField_4.setBounds(77, 196, 86, 20);
+		panel.add(textField_4);
+		textField_4.setColumns(10);
+		
+		textField_5 = new JTextField();
+		textField_5.setBounds(239, 40, 86, 20);
+		panel.add(textField_5);
+		textField_5.setColumns(10);
+		
+		textField_6 = new JTextField();
+		textField_6.setBounds(239, 73, 86, 20);
+		panel.add(textField_6);
+		textField_6.setColumns(10);
+		
+		textField_7 = new JTextField();
+		textField_7.setBounds(239, 110, 86, 20);
+		panel.add(textField_7);
+		textField_7.setColumns(10);
+		
+		textField_8 = new JTextField();
+		textField_8.setBounds(239, 149, 86, 20);
+		panel.add(textField_8);
+		textField_8.setColumns(10);
+		// TODO Auto-generated constructor stub
+	}
+}

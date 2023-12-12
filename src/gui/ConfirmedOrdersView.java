3744d96838a604add7461ff0576d25f2a7ee480f
rasmuslarsendma <115786715+rasmuslarsendma@users.noreply.github.com>
2023-12-12 22:36:57 +0100

    Create ConfirmedOrdersView.java

---
+++ src/gui/ConfirmedOrdersView.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package gui;
+
+/**
+ * @author Rasmus Larsen, Viktor Dorph, Johannes Jensen, Malik Agerbæk, Shemon Chowdhury 
+ *
+ */
+public class ConfirmedOrdersView {
+
+	/**
+	 * 
+	 */
+	public ConfirmedOrdersView() {
+		// TODO Auto-generated constructor stub
+	}
+
+}

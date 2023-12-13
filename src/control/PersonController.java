0dfe3155e375a546aea0bc69db66b9d41bbd26f7
viktor-bd <viktordorph@hotmail.com>
2023-12-13 13:29:51 +0100

    Add lookUpCustomerInDB method

--- src/control/PersonController.java
+++ src/control/PersonController.java
@@ -3,7 +3,9 @@
  */
 package control;
 
+import dataaccesslayer.DataAccessException;
 import dataaccesslayer.PersonDB;
+import model.Customer;
 
 /**
  * @author 
@@ -19,5 +21,13 @@ public class PersonController {
 		// TODO Auto-generated constructor stub
 		this.personDB = personDB;
 	}
+
+	public Customer lookUpCustomerInDB(String validPhoneNumber) throws DataAccessException {
+		// TODO Auto-generated method stub
+		Customer customerFound = personDB.lookUpCustomerInDB(validPhoneNumber);
+		return customerFound;
+	}
+
+	
 
 }
--- src/dataaccesslayer/PersonDB.java
+++ src/dataaccesslayer/PersonDB.java
@@ -1,6 +1,40 @@
 package dataaccesslayer;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import model.Customer;
 public class PersonDB implements PersonDBIF {
-    // TODO Implement this class
+	public Customer lookUpCustomerInDB(String validPhoneNumber) throws DataAccessException {
+		
+		try {
+			findCustomerByPhoneNumber.setString(1, validPhoneNumber);
+			ResultSet resultSet = findCustomerByPhoneNumber.executeQuery();
+			Customer customer = null;
+			if (resultSet.next()) {
+				customer = buildObject(resultSet);
+				customer.setPhoneNumber(validPhoneNumber);
+			}
+			return customer;
+		}catch (SQLException e) {
+			throw new DataAccessException(e, "Could not find customer by phone number = " + validPhoneNumber);
+		}
+		
+	}
+	private Customer buildObject(ResultSet resultSet) throws SQLException {
+		Customer customer = new Customer(
+				resultSet.getInt("person_id"),
+				resultSet.getString("firsName"), 
+				resultSet.getString("lastName"),
+				resultSet.getString("phoneNumber"),
+				resultSet.getString("zipcode"),
+				resultSet.getString("email"),
+				resultSet.getString("address"),
+				resultSet.getBoolean("isBusiness")
+				);
+
+		System.out.println("Object succesfully created " + customer.getFirstName());
+		return customer;
 
 }

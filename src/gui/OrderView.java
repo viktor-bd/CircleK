f4cd56f06d0cac10e80ce63bee6b9da725a73ebc
rasmuslarsendma <115786715+rasmuslarsendma@users.noreply.github.com>
2023-12-13 12:09:52 +0100

    Update OrderView.java

--- src/gui/OrderView.java
+++ src/gui/OrderView.java
@@ -6,6 +6,7 @@ package gui;
 import java.awt.Component;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.time.LocalDate;
 
 import javax.swing.JButton;
@@ -42,7 +43,7 @@ public class OrderView extends JFrame {
 		JPanel panel = new JPanel();
 		panel.setBounds(172, 5, 89, 23);
 		getContentPane().add(panel);
-		panel.setLayout(null);
+		panel.setLayout(null);			
 
 		btnCreateOrder = new JButton("Opret ordre");
 		btnCreateOrder.setBounds(172, 52, 89, 23);
@@ -54,32 +55,76 @@ public class OrderView extends JFrame {
 		getContentPane().add(btnViewOrders);
 
 		btnConfirmDenyOrders = new JButton("Godkend/Afvis ordre");
+		btnConfirmDenyOrders.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				confirmDenyClicked();
+			}
+		});
 		btnConfirmDenyOrders.setBounds(172, 120, 89, 23);
 		getContentPane().add(btnConfirmDenyOrders);
 		btnLogOut = new JButton("Log ud(?)");
 		btnLogOut.setBounds(172, 154, 89, 23);
 		getContentPane().add(btnLogOut);
-		
+		btnLogOut.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				logOutClicked();
+			}
+		});
+
 	}
 
+	/**
+	 * Makes the OrderView visible
+	 * 
+	 * @param orderView
+	 */
 	public void run(OrderView orderView) {
 		orderView.setVisible(true);
 	}
 
+	/*
+	 * Opens the date picker to create order
+	 */
 	public void btnCreateOrderClicked(ActionEvent e) {
 		openDatePickerView();
 	}
 
 	/**
-	 * 
+	 * Opens a DatePickerView when createOrderClicked
 	 */
 	private void openDatePickerView() {
 		DatePickerView dpv = new DatePickerView();
 		dpv.setVisible(true);
+		clearWindow();
 	}
 
-	public void enableProductView() {
-		ProductView productView = new ProductView();
-		productView.setVisible(true);
+	public void confirmDenyClicked() {
+		UnconfirmedOrderView unconfirmedView = new UnconfirmedOrderView();
+		unconfirmedView.setVisible(true);
+		clearWindow();
+	}
+
+	// Resets to loginscreen and clears the employee logged in
+	public void logOutClicked() {
+		EmployeeLoginView employeeLogin = new EmployeeLoginView();
+		employeeLogin.resetEmployee();
+		employeeLogin.setVisible(true);
+		clearWindow();
+	}
+
+	/**
+	 * Closes current window
+	 */
+	public void clearWindow() {
+		this.setVisible(false);
+		this.dispose();
+	}
+
+	/**
+	 * Updates the display
+	 */
+	private void updateDisplay() {
+		this.revalidate();
+		this.repaint();
 	}
 }

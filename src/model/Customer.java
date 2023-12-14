361bfb8bd2931f296839fe73b44ff534f13af70b
viktor-bd <viktordorph@hotmail.com>
2023-12-14 11:21:27 +0100

    Format

--- src/model/Customer.java
+++ src/model/Customer.java
@@ -4,12 +4,14 @@
 package model;
 
 /**
- * @author Rasmus Larsen, Viktor Dorph, Johannes Jensen, Malik Agerbæk, Shemon Chowdhury 
+ * @author Rasmus Larsen, Viktor Dorph, Johannes Jensen, Malik Agerbæk, Shemon
+ *         Chowdhury
  *
  */
 public class Customer extends Person {
-    private int customerId; //TODO Same as employeeID, check whether its needed to duplicate to differentiate: Fix constructor if wanted
-    private Boolean isBusiness; // Allow null, 1 for business, 0 for private
+	private int customerId; // TODO Same as employeeID, check whether its needed to duplicate to
+							// differentiate: Fix constructor if wanted
+	private Boolean isBusiness; // Allow null, 1 for business, 0 for private
 
 	// Constructor
 	public Customer(int personId, String firstName, String lastName, String phoneNumber, String zipcode, String email,
@@ -19,30 +21,31 @@ public class Customer extends Person {
 		isBusiness = false; // Redundant, as NULL from db return is treated as false anyhow.
 	}
 
-    // Getter and Setter for customerId
-    public int getCustomerId() {
-        return customerId;
-    }
+	// Getter and Setter for customerId
+	public int getCustomerId() {
+		return customerId;
+	}
 
-    public void setCustomerId(int customerId) {
-        this.customerId = customerId;
-    }
+	public void setCustomerId(int customerId) {
+		this.customerId = customerId;
+	}
 
-    // Getter and Setter for isBusiness
-    public Boolean getIsBusiness() {
-        return isBusiness;
-    }
+	// Getter and Setter for isBusiness
+	public Boolean getIsBusiness() {
+		return isBusiness;
+	}
 
-    public void setIsBusiness(Boolean isBusiness) {
-        this.isBusiness = isBusiness;
-    }
+	public void setIsBusiness(Boolean isBusiness) {
+		this.isBusiness = isBusiness;
+	}
 
-    /**
-     * Returns whether or not the customer is a business.
-     *
-     * @return true if the customer is a business, false if private. This can be null (!= null && isBusiness)
-     */
-    public boolean isBusiness() {
-        return isBusiness;
-    }
+	/**
+	 * Returns whether or not the customer is a business.
+	 *
+	 * @return true if the customer is a business, false if private. This can be
+	 *         null (!= null && isBusiness)
+	 */
+	public boolean isBusiness() {
+		return isBusiness;
+	}
 }

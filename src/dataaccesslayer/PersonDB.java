456158ba9d059d59c1d1f2362e6ddb603c4402f0
viktor-bd <viktordorph@hotmail.com>
2023-12-14 11:21:37 +0100

    Format

--- src/dataaccesslayer/PersonDB.java
+++ src/dataaccesslayer/PersonDB.java
@@ -15,17 +15,19 @@ public class PersonDB implements PersonDBIF {
 	private static final String findCustomerByPhoneNumberQuery = "SELECT person_id, first_name, last_name, phonenumber, zipcode, email, address, isBusiness FROM Person LEFT JOIN Customer ON Customer.customer_id = Person.person_id WHERE phonenumber = ?";
 
 	private PreparedStatement findCustomerByPhoneNumber;
-	
+
 	public PersonDB() throws DataAccessException {
-		 try {
-				findCustomerByPhoneNumber = DBConnection.getInstance().getDBcon().prepareStatement(findCustomerByPhoneNumberQuery);
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				throw new DataAccessException(e, "Could not prepare statement");
-			}
+		try {
+			findCustomerByPhoneNumber = DBConnection.getInstance().getDBcon()
+					.prepareStatement(findCustomerByPhoneNumberQuery);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			throw new DataAccessException(e, "Could not prepare statement");
+		}
 	}
+
 	public Customer lookUpCustomerInDB(String validPhoneNumber) throws DataAccessException {
-		
+
 		try {
 			findCustomerByPhoneNumber.setString(1, validPhoneNumber);
 			ResultSet resultSet = findCustomerByPhoneNumber.executeQuery();
@@ -35,11 +37,12 @@ public class PersonDB implements PersonDBIF {
 				customer.setPhoneNumber(validPhoneNumber);
 			}
 			return customer;
-		}catch (SQLException e) {
+		} catch (SQLException e) {
 			throw new DataAccessException(e, "Could not find customer by phone number = " + validPhoneNumber);
 		}
-		
+
 	}
+
 	private Customer buildObject(ResultSet resultSet) throws SQLException {
 		Customer customer = new Customer(resultSet.getInt("person_id"), resultSet.getString("first_name"),
 				resultSet.getString("last_name"), resultSet.getString("phonenumber"), resultSet.getString("zipcode"),
@@ -53,8 +56,7 @@ public class PersonDB implements PersonDBIF {
 
 		System.out.println("Object succesfully created " + customer.getFirstName());
 		return customer;
-			
-}
-    
+
+	}
 
 }

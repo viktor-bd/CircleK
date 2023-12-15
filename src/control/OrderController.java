2014f28c35b55ece67478e447c4a59a1b013183c
viktor-bd <viktordorph@hotmail.com>
2023-12-15 23:37:46 +0100

    Format

--- src/control/OrderController.java
+++ src/control/OrderController.java
@@ -6,51 +6,45 @@ import model.*;
 
 import java.sql.SQLException;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 
 public class OrderController {
 
+	private OrderDB orderDB;
 
-    private OrderDB orderDB;
+	public OrderController() throws DataAccessException {
+		this.orderDB = new OrderDB();
+	}
 
+	public Order createOrder(int orderID, boolean pickUpStatus, LocalDateTime pickupDate, boolean isPaid,
+			Customer customer, Employee employee) {
+		Order newOrder = new Order(orderID, pickUpStatus, pickupDate, isPaid, customer, employee);
+		return newOrder;
+	}
 
-    public OrderController() throws DataAccessException {
-        this.orderDB = new OrderDB();
-    }
+	public Order saveOrder(Order order) throws SQLException {
+		orderDB.saveOrder(order);
+		return order;
+	}
 
-    public Order createOrder(int orderID, boolean pickUpStatus, LocalDateTime pickupDate, boolean isPaid, Customer customer, Employee employee) {
-        Order newOrder = new Order(orderID, pickUpStatus, pickupDate, isPaid, customer, employee);
-        return newOrder;
-    }
+	public Order addOrderLineToOrder(OrderLine orderLine, Order order) {
+		order.addOrderLine(orderLine);
+		return order;
+	}
 
-    public Order saveOrder(Order order) throws SQLException {
-    	System.out.println("OC SaveOrder 27 " +order.getSizeOfOrderLines());
-        orderDB.saveOrder(order); // Call orderDB to save the order
-        return order;
-    }   
-
-
-    public Order addOrderLineToOrder(OrderLine orderLine, Order order) {
-        order.addOrderLine(orderLine);
-        return order;
-    }
-
-    public OrderLine createOrderLine(Product product, int quantity) {
-        OrderLine orderLine = new OrderLine(quantity, product);
-        return orderLine;
-    }
+	public OrderLine createOrderLine(Product product, int quantity) {
+		OrderLine orderLine = new OrderLine(quantity, product);
+		return orderLine;
+	}
 
 	public void confirmOrder(Order order) {
 		order.setPickUpStatus(true);
 	}
 
 	public void addCustomerToOrder(Customer customer, Order order) {
-		// TODO mangler lige logic skal nok skrive det - Viktor
 		order.setCustomer(customer);
 	}
 
-	public void testaddEmployeeToOrder(Employee testEmployee , Order newOrder) {
-		// TODO Auto-generated method stub
+	public void testaddEmployeeToOrder(Employee testEmployee, Order newOrder) {
 		newOrder.setEmployee(testEmployee);
 	}
 

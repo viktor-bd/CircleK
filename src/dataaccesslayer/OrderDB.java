44a2467f4443990daa13c266e667e0e5f26cb6fe
viktor-bd <viktordorph@hotmail.com>
2023-12-15 18:15:17 +0100

    Outcomment debug code

--- src/dataaccesslayer/OrderDB.java
+++ src/dataaccesslayer/OrderDB.java
@@ -62,14 +62,14 @@ public class OrderDB implements OrderDBIF {
 		}
 	}
 
-	private void insertOrderOrderLine(ArrayList<Integer> orderLineID, int orderID) {
+	/*private void insertOrderOrderLine(ArrayList<Integer> orderLineID, int orderID) {
 		// TODO Auto-generated method stub
 		for (int j = 0; j < orderLineID.size(); j++) {
 
 			try {
 				insertOrderOrderLine.setInt(1, orderID);
 				insertOrderOrderLine.setInt(2, orderLineID.get(j));
-				
+
 				insertOrderOrderLine.executeUpdate();
 			} catch (SQLException e) {
 				// TODO Auto-generated catch block

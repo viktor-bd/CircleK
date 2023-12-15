3d86d30f1a5b7bc38369cad17055f68a509e4ee5
viktor-bd <viktordorph@hotmail.com>
2023-12-15 23:49:45 +0100

    Remove unnused namespaces

--- src/dataaccesslayer/OrderDB.java
+++ src/dataaccesslayer/OrderDB.java
@@ -1,17 +1,11 @@
 package dataaccesslayer;
 
-import java.lang.reflect.Array;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.sql.Timestamp;
 import java.util.ArrayList;
-import java.util.List;
-
-import dataaccesslayer.DBConnection;
-import dataaccesslayer.DataAccessException;
 import model.Order;
 import model.OrderLine;
 
